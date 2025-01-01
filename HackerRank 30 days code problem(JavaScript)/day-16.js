c445ef32d80c51fbdff116947172b51a6be6a9bb
ARYAN KADYAN <68529739+Aryankadyan@users.noreply.github.com>
2025-01-02 02:38:11 +0530

    Create day-16.js

---
+++ HackerRank 30 days code problem(JavaScript)/day-16.js
@@ -0,0 +1,14 @@
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+function main() {
+    const S = readLine();
+try{
+    isNaN(Number(S)) ? error: console.log(S)
+}
+catch(err){
+    console.log('Bad String')
+}
+}
