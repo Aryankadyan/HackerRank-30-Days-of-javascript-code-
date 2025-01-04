bcfef1fe0982bc4f3d733931d79dfeadda45a003
ARYAN KADYAN <68529739+Aryankadyan@users.noreply.github.com>
2025-01-04 22:50:35 +0530

    Create day-24.js

---
+++ HackerRank 30 days code problem(JavaScript)/day-24.js
@@ -0,0 +1,12 @@
+//Write your code here
+      let prev = head;
+      
+      while(prev){
+        let next = prev.next;
+        
+        (next && prev.data == next.data)
+        ? (prev.next = next.next)
+        : (prev = prev.next)
+      }
+      return head;
+    }
