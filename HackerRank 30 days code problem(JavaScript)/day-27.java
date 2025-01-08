50fbb84733f1962b0db88196e463ed311cf517d3
ARYAN KADYAN <68529739+Aryankadyan@users.noreply.github.com>
2025-01-08 08:24:50 +0530

    Create day-27.java

---
+++ HackerRank 30 days code problem(JavaScript)/day-27.java
@@ -0,0 +1,22 @@
+static class TestDataEmptyArray{
+        public static int[] get_array(){
+            return new int[0];
+        }
+    }
+    static class TestDataUniqueValues{
+        public static int[] get_array(){
+            return new int[]{11,8,9,3,7,86,15};
+        }
+        public static int get_expected_result(){
+            return 3;
+        }
+    }
+    static class TestDataExactlyTwoDifferentMinimums{
+        public static int[] get_array(){
+            return new int[]{17,11,450,69,87,16,11};      
+        }
+        public static int get_expected_result(){
+            return 1;
+        }
+        
+    }
